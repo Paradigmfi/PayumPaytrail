fb4aa116fc73946506a28beddc6353648351663c
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2016-03-03 19:15:59 +0000

    [api] impl create Payment api call.

--- Api.php
+++ Api.php
@@ -3,6 +3,7 @@ namespace Paradigmfi\PayumPaytrail;
 
 use GuzzleHttp\Psr7\Request;
 use Payum\Core\Bridge\Guzzle\HttpClientFactory;
+use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\Exception\Http\HttpException;
 use Payum\Core\HttpClientInterface;
 
@@ -26,7 +27,10 @@ class Api
      */
     public function __construct(array $options, HttpClientInterface $client = null)
     {
+        $options = ArrayObject::ensureArrayObject($options);
+        $options->validateNotEmpty(['merchantId', 'merchantSecret']);
         $this->options = $options;
+
         $this->client = $client ?: HttpClientFactory::create();
     }
 
@@ -35,26 +39,41 @@ class Api
      *
      * @return array
      */
-    protected function doRequest($method, array $fields)
+    public function createPayment(array $fields)
     {
-        $headers = [];
-
-        $request = new Request($method, $this->getApiEndpoint(), $headers, http_build_query($fields));
-
-        $response = $this->client->send($request);
-
-        if (false == ($response->getStatusCode() >= 200 && $response->getStatusCode() < 300)) {
-            throw HttpException::factory($request, $response);
-        }
-
-        return $response;
+        return $this->doRequest('POST', 'https://payment.paytrail.com/api-payment/create', $fields);
     }
 
     /**
-     * @return string
+     * @param array $fields
+     *
+     * @return array
      */
-    protected function getApiEndpoint()
+    protected function doRequest($method, $url, array $fields)
     {
-        return $this->options['sandbox'] ? 'http://sandbox.example.com' : 'http://example.com';
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'X-Verkkomaksut-Api-Version' => 1,
+            'Authorization' => 'Basic '.base64_encode($this->options['merchantId'].':'.$this->options['merchantSecret']),
+        ];
+
+        $request = new Request($method, $url, $headers, json_encode($fields));
+
+        $response = $this->client->send($request);
+
+        if (false == (
+            ($response->getStatusCode() >= 200 && $response->getStatusCode() < 300) ||
+            $response->getStatusCode() == 400
+        )) {
+            throw HttpException::factory($request, $response);
+        }
+
+        if (null === $body = json_decode($response->getBody(), true)) {
+            throw HttpException::factory($request, $response);
+        }
+
+
+        return $body;
     }
 }
